4edeeade51b62ee7ca20fa35adf1174232497289
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2022-05-13 13:59:48 +0100

    Update entrypoint.sh

--- .github/actions/esp-build/entrypoint.sh
+++ .github/actions/esp-build/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
 echo "hello $1"
-su root
+# su root
 id -u -n
-ls /home/esp -al
-chown -R root:root /home/esp
-ls /home/esp -al
+#ls /home/esp -al
+#chown -R root:root /home/esp
+#ls /home/esp -al
 # /home/esp/export-rust.sh
 # env
 # /home/esp/.cargo/bin/rustup toolchain list
